1d2a987272ddd03ae038c6837fd575f2b1213401
YasminLuna <65403619+YasminLuna@users.noreply.github.com>
2026-09-14 18:05:10 -0300

    infra: configure auth remote state and random provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,5 +1,8 @@
 terraform {
   required_version = ">= 1.6.0"
+
+  backend "s3" {}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,6 +11,10 @@ terraform {
     archive = {
       source  = "hashicorp/archive"
       version = "~> 2.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
     }
   }
 }
