bdc0218f1b8061ee93b4a64304de10318df2d2f3
YasminLuna <65403619+YasminLuna@users.noreply.github.com>
2026-09-14 11:07:16 -0300

    fix: corrige sintaxe das variáveis Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,29 @@
-variable "aws_region" { type = string, default = "us-east-1" }
-variable "environment" { type = string, default = "hml" }
-variable "database_url" { type = string, sensitive = true }
-variable "jwt_secret" { type = string, sensitive = true }
-variable "lambda_subnet_ids" { type = list(string), default = [] }
-variable "lambda_security_group_ids" { type = list(string), default = [] }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "environment" {
+  type    = string
+  default = "hml"
+}
+
+variable "database_url" {
+  type      = string
+  sensitive = true
+}
+
+variable "jwt_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "lambda_subnet_ids" {
+  type    = list(string)
+  default = []
+}
+
+variable "lambda_security_group_ids" {
+  type    = list(string)
+  default = []
+}
